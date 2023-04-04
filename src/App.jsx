ddb224f8a072bd7a56fb1bc9fff89513e27af8f8
jjkim112 <jjkim112@naver.com>
2023-04-04 11:18:36 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      Hello, React
+    </div>
+  );
 }
 
 export default App;
